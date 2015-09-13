17be8fadb67053920cd7f8f8ecbf5e0a2a88f6ce
Ben Ockmore <ben.sput@gmail.com>
2015-09-13 02:15:17 +0100

    Moved tests to default for mocha (test), and added missing bookshelf module

--- test/bookshelf.js
+++ test/bookshelf.js
@@ -4,7 +4,7 @@ var knex = require('knex')({
     host     : '127.0.0.1',
     user     : 'bookbrainz',
     password : 'bookbrainz',
-    database : 'bookbrainz'
+    database : 'bookbrainz_test'
   }
 });
 
