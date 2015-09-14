9b011cd5b82ccf9bb34cd8b563764dc4e67aab57
Ben Ockmore <ben.sput@gmail.com>
2015-09-14 22:55:24 +0100

    Properly deal with _-prefixed fields in camelCase conversion

--- util.js
+++ util.js
@@ -3,14 +3,28 @@ _.str = require('underscore.string');
 
 module.exports.snakeToCamel = function(attrs) {
   return _.reduce(attrs, function(result, val, key) {
-    result[_.str.camelize(key)] = val;
+    var newKey;
+    if (key.indexOf('_') === 0) {
+      newKey = '_' + _.str.camelize(key.substr(1));
+    } else {
+      newKey = _.str.camelize(key);
+    }
+
+    result[newKey] = val;
     return result;
   }, {});
 };
 
 module.exports.camelToSnake = function(attrs) {
   return _.reduce(attrs, function(result, val, key) {
-    result[_.str.underscored(key)] = val;
+    var newKey;
+    if (key.indexOf('_') === 0) {
+      newKey = '_' + _.str.underscored(key.substr(1));
+    } else {
+      newKey = _.str.underscored(key);
+    }
+
+    result[newKey] = val;
     return result;
   }, {});
 };
