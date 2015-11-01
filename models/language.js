4676319efb184c0407ad57c754522f6c09df0ca4
Ben Ockmore <ben.sput@gmail.com>
2015-11-01 20:27:06 +0100

    Added language model and basic test

---
+++ models/language.js
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2015  Ben Ockmore
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+'use strict';
+
+const util = require('../util');
+
+let Language = null;
+const _ = require('underscore');
+
+function formatWithISOFields(attrs) {
+	const REPLACEMENTS = {
+		isoCode1: 'iso_code_1',
+		isoCode2t: 'iso_code_2t',
+		isoCode2b: 'iso_code_2b',
+		isoCode3: 'iso_code_3'
+	};
+
+	return util.camelToSnake(
+		_.reduce(attrs, (result, val, key) => {
+			const newKey = _.has(REPLACEMENTS, key) ?
+				REPLACEMENTS[key] : key;
+			result[newKey] = val;
+			return result;
+		}, {})
+	);
+}
+
+module.exports = function(bookshelf) {
+	if (!Language) {
+		Language = bookshelf.Model.extend({
+			tableName: 'musicbrainz.language',
+			idAttribute: 'id',
+			parse: util.snakeToCamel,
+			format: formatWithISOFields
+		});
+
+		Language = bookshelf.model('Language', Language);
+	}
+	return Language;
+};
---
+++ test/testLanguage.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2015  Ben Ockmore
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+'use strict';
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const expect = chai.expect;
+const Promise = require('bluebird');
+const util = require('../util');
+
+const orm = require('./bookshelf');
+const Bookshelf = orm.bookshelf;
+
+const Language = orm.Language;
+
+chai.use(chaiAsPromised);
+
+/* eslint camelcase: 0 */
+
+describe('Language model', function setupData() {
+	afterEach(function destroyData() {
+		return Promise.all([
+			Bookshelf.knex.raw('TRUNCATE musicbrainz.language CASCADE')
+		]);
+	});
+
+	it('should return a JSON object with correct keys when saved', function() {
+		// Construct EntityRevision, add to Entity, then save
+		const languageAttribs = {
+			name: 'English', isoCode2t: 'eng', isoCode2b: 'eng',
+			isoCode3: 'eng', isoCode1: 'en', frequency: 1
+		};
+
+		const languagePromise = new Language(languageAttribs).save()
+		.then((model) => model.refresh().then(util.fetchJSON));
+
+		return expect(languagePromise).to.eventually.have.all.keys([
+			'id', 'name', 'isoCode2t', 'isoCode2b', 'isoCode1', 'frequency',
+			'isoCode3'
+		]);
+	});
+});
