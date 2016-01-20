bdce5c91ca6ecae2774e11872b6b490aa84d3caa
Ben Ockmore <ben.sput@gmail.com>
2016-01-20 12:11:24 +0000

    Switch to lodash string methods and drop dependency on underscore.string

--- util.js
+++ util.js
@@ -1,17 +1,16 @@
 'use strict';
 
 const _ = require('lodash');
-_.str = require('underscore.string');
 
 module.exports.snakeToCamel = (attrs) => {
 	return _.reduce(attrs, (result, val, key) => {
 		let newKey;
 
 		if (key.indexOf('_') === 0) {
-			newKey = `_${_.str.camelize(key.substr(1))}`;
+			newKey = `_${_.camelCase(key.substr(1))}`;
 		}
 		else {
-			newKey = _.str.camelize(key);
+			newKey = _.camelCase(key);
 		}
 
 		result[newKey] = val;
@@ -25,10 +24,10 @@ module.exports.camelToSnake = (attrs) => {
 		let newKey;
 
 		if (key.indexOf('_') === 0) {
-			newKey = `_${_.str.underscored(key.substr(1))}`;
+			newKey = `_${_.snakeCase(key.substr(1))}`;
 		}
 		else {
-			newKey = _.str.underscored(key);
+			newKey = _.snakeCase(key);
 		}
 
 		result[newKey] = val;
