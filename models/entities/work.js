1d68a4fb92ab838cfe2e943b731f23b209f56760
Ben Ockmore <ben.sput@gmail.com>
2016-02-14 00:41:35 +0000

    Added Edition, Publication, Publisher and Work models and Edition tests

---
+++ models/entities/work.js
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2015-2016  Ben Ockmore
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+'use strict';
+
+const util = require('../../util');
+
+module.exports = (bookshelf) => {
+	const Work = bookshelf.Model.extend({
+		tableName: 'bookbrainz.work',
+		idAttribute: 'bbid',
+		parse: util.snakeToCamel,
+		format: util.camelToSnake,
+		initialize() {
+			this.on('fetching', (model, col, options) => {
+				// If no revision is specified, fetch the master revision
+				if (!model.get('revisionId')) {
+					options.query.where({master: true});
+				}
+			});
+		},
+		annotation() {
+			return this.belongsTo('Annotation', 'annotation_id');
+		},
+		disambiguation() {
+			return this.belongsTo('Disambiguation', 'disambiguation_id');
+		},
+		defaultAlias() {
+			return this.belongsTo('Alias', 'default_alias_id');
+		},
+		relationshipSet() {
+			return this.belongsTo('RelationshipSet', 'relationship_set_id');
+		},
+		aliasSet() {
+			return this.belongsTo('AliasSet', 'alias_set_id');
+		},
+		identifierSet() {
+			return this.belongsTo('IdentifierSet', 'identifier_set_id');
+		},
+		revision() {
+			return this.belongsTo('WorkRevision', 'revision_id');
+		}
+	});
+
+	return bookshelf.model('Work', Work);
+};
