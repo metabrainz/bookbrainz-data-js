8922c1d4194884f28fe603e4e2f075dee568901f
Ben Ockmore <ben.sput@gmail.com>
2016-02-14 00:11:27 +0000

    Bumped copyright year in other recently changed files

--- models/data/creatorData.js
+++ models/data/creatorData.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016  Ben Ockmore
+ * Copyright (C) 2015-2016  Ben Ockmore
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
