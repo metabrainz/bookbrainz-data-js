9429758264f08030280e2fbe1966247d3fe6fe3e
Ben Ockmore <ben.sput@gmail.com>
2016-03-02 14:49:21 +0000

    Added missing model for revision notes

---
+++ models/note.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2015  Ben Ockmore
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+'use strict';
+
+const util = require('../util');
+
+module.exports = (bookshelf) => {
+	const Note = bookshelf.Model.extend({
+		tableName: 'bookbrainz.note',
+		idAttribute: 'id',
+		parse: util.snakeToCamel,
+		format: util.camelToSnake,
+		author() {
+			return this.belongsTo('Editor', 'author_id');
+		},
+		revision() {
+			return this.belongsTo('Revision', 'revision_id');
+		}
+	});
+
+	return bookshelf.model('Note', Note);
+};
