6525e5d7ca6cbcf0ca5fc88a6868ff5aac84ed7d
Max Prettyjohns <heshoots9999@gmail.com>
2016-05-31 01:22:17 +0100

    Add new test file for AchievementType

---
+++ test/testAchievementType.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2016  Max Prettyjohns
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+'use strict';
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const util = require('../util');
+const Bookshelf = require('./bookshelf');
+const AchievementType = require('../index').AchievementType;
+
+describe('AchievementType model', () => {
+	afterEach(() => {
+		return util.truncateTables(Bookshelf, ['bookbrainz.AchievementType']);
+	});
+
+	it('should return a JSON object with correct keys when saved', () => {
+		const achievementTypePromise = new AchievementType({name: 'Test'})
+			.save(null, {method: 'insert'})
+			.then((model) => model.refresh())
+			.then((achievementType) => achievementType.ToJSON());
+
+		return expect(achievementTypePromise).to.eventually.have.all.keys([
+			'id', 'name', 'description', 'badge_url'
+		]);
+	});
+});
