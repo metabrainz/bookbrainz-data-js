02741957ab0e96280e638af0b5b28b8baffc0324
Max Prettyjohns <heshoots9999@gmail.com>
2016-06-14 14:25:47 +0100

    Add model for achievement rank table

---
+++ models/achievementRank.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const util = require('../util');
+
+module.exports = (bookshelf) => {
+	const AchievementRank = bookshelf.model.extend({
+		tableName: 'bookbrainz.achievement_rank',
+		idAttribute: 'id',
+		parse: util.snakeToCamel,
+		format: util.camelToSnake,
+		editor() {
+			return this.belongsTo('Editor', 'editor_id');
+		},
+		achievement() {
+			return this.belongsTo('AchievementType', 'achievement_id');
+		}
+	});
+
+	return bookshelf.model('AchievementRank', AchievementRank);
+};
