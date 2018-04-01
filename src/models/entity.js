694aa8dec0c877c38978cf2154f71b6e6df4f8ce
Shivam Tripathi <shivamtripathi0108@gmail.com>
2018-04-01 21:47:30 +0530

    feat(entity): Add function to calculate most recent revisions
    Add a function mostRecentEntityRevisions which given entityType and limit
    computes the most recent revisions of that entity type limited by the limit
    parameter.

--- src/models/entity.js
+++ src/models/entity.js
@@ -18,6 +18,11 @@
 
 import {camelToSnake, snakeToCamel} from '../util';
 
+import Immutable from 'immutable';
+import Promise from 'bluebird';
+import _ from 'lodash';
+import {mapRevisionsToAlias} from './revision';
+
 
 export function entity(bookshelf) {
 	const Entity = bookshelf.Model.extend({
@@ -31,4 +36,75 @@ export function entity(bookshelf) {
 	});
 
 	return bookshelf.model('Entity', Entity);
+}
+
+/**
+ * Produce a mapping of revisionIds and their default alias names.
+ * @param {object} bookshelf - The Bookshelf object.
+ * @returns {function} The returned function which inputs entityType and limit
+ * 					   signifying the number of recent entities to be retrieved.
+ */
+export function mostRecentEntityRevisions(bookshelf) {
+	return (entityType, limit) => {
+		const rawSql = `
+			SELECT editor.name as editor_name,
+				   alias.name as alias_name,
+				   revision.id as revision_id,
+				   revision.created_at as created_at,
+				   revision_parent.parent_id as parent_id
+			  FROM ${entityType} as en
+			  JOIN revision ON revision.id = en.revision_id
+			  JOIN editor ON editor.id = revision.author_id
+		 LEFT JOIN alias on alias.id = default_alias_id
+		 LEFT JOIN revision_parent on revision_parent.child_id = revision.id
+		  ORDER BY revision.created_at DESC
+			 LIMIT ${limit};
+		`;
+
+		// Query the database to get the revisions
+		const rowsPromise = bookshelf.knex.raw(rawSql).then(val =>
+			val.rows.map((rawQueryResult) => {
+				// Convert query keys to camelCase
+				const queriedResult = _.mapKeys(
+					rawQueryResult,
+					(value, key) => _.camelCase(key)
+				);
+
+				// Add entity type to the queried result
+				queriedResult.type = entityType;
+				// Add action to the queried result
+				if (queriedResult.parentId === null) {
+					queriedResult.action = 'CREATE';
+				}
+				else if (queriedResult.aliasName === null) {
+					queriedResult.action = 'DELETE';
+				}
+				else {
+					queriedResult.action = 'EDIT';
+				}
+				return queriedResult;
+			}));
+
+		// Query the database to get the names of the parentIds of the revisions
+		const parentNamesPromise = rowsPromise.then((rows) =>
+			mapRevisionsToAlias(bookshelf)(
+				entityType,
+				rows.map(row => row.parentId)
+			));
+
+		return Promise.join(
+			rowsPromise,
+			parentNamesPromise,
+			(rows, parentNames) =>
+				// Add the alias name of the parentId if the action is DELETE
+				Immutable.fromJS(rows.map(row => {
+					if (row.action === 'DELETE') {
+						row.aliasName = parentNames.get(
+							row.parentId.toString()
+						);
+					}
+					return row;
+				}))
+		);
+	};
 }
