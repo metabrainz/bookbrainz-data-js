859cee1f0bda8e8b2d3f80c70cfcd8150f48aa6a
Shivam Tripathi <shivamtripathi0108@gmail.com>
2018-07-10 23:07:40 +0530

    feat(func/derivedSets): Update language set id function

--- src/func/derivedSets.js
+++ src/func/derivedSets.js
@@ -21,38 +21,51 @@ import {
 	getUnchangedItems
 } from './set';
 import _ from 'lodash';
+import {camelToSnake} from '../util';
 
 
-export async function updateLanguageSet(orm, transacting, oldSet, newSetItems) {
-	function cmpFunc(obj, other) {
+export async function updateLanguageSet(transacting, currentEntity,
+	newLanguageItems) {
+	function cmpLanguageItems(obj, other) {
 		return obj === other;
 	}
 
+	const oldLangaugeSetId = currentEntity && currentEntity.languageSetId;
 
-	const {LanguageSet} = orm;
-	let oldSetItems =
-	oldSet ? await oldSet.related('languages').toJSON() : [];
+	// All errors are expected to be handled on the usage side
+	if (oldLangaugeSetId) {
+		// Extract old language_ids
+		const oldLanguageItemsMap = await transacting.select('language_id')
+			.from('language_set__language')
+			.where('set_id', oldLangaugeSetId);
 
-	if (_.isEmpty(oldSetItems) && _.isEmpty(newSetItems)) {
-		return oldSet;
+		const oldLanguageItems = _.map(oldLanguageItemsMap, 'language_id');
+
+		// Check if there has been any change
+		const addedItems = getAddedItems(
+			oldLanguageItems, newLanguageItems, cmpLanguageItems);
+		const removedItems = getRemovedItems(
+			oldLanguageItems, newLanguageItems, cmpLanguageItems);
+
+		const isSetUnmodified =
+			_.isEmpty(removedItems) && _.isEmpty(addedItems);
+		if (isSetUnmodified) {
+			return oldLangaugeSetId;
+		}
 	}
 
-	const addedItems = getAddedItems(oldSetItems, newSetItems, cmpFunc);
-	const removedItems = getRemovedItems(oldSetItems, newSetItems, cmpFunc);
-	const unchangedItems = getUnchangedItems(oldSetItems, newSetItems, cmpFunc);
+	const [setId] = await transacting.insert({})
+		.into('language_set')
+		.returning('id');
 
-	const isSetUnmodified = _.isEmpty(addedItems) && _.isEmpty(removedItems);
-
-	if (isSetUnmodified) {
-		// No action - set has not changed
-		return oldSet;
-	}
-
-	const newSet = await createNewSetWithItems(
-		orm, transacting, LanguageSet, unchangedItems, addedItems
+	const queryArgs = newLanguageItems.map(item =>
+		camelToSnake({languageId: item, setId})
 	);
 
-	return newSet.save(null, {transacting});
+	await transacting.insert(queryArgs)
+		.into('language_set__language');
+
+	return setId;
 }
 
 export async function updateReleaseEventSet(orm, transacting, oldSet,
