a159c420c0b4bb58c18b613160590dcd144afdde
Shivam Tripathi <shivamtripathi0108@gmail.com>
2018-07-30 00:45:12 +0530

    feat(func/annotation): Add flow and jsdocs

--- src/func/annotation.js
+++ src/func/annotation.js
@@ -16,9 +16,22 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+// @flow
 
+import type {Transaction} from './types';
+
+
+/**
+ * @param  {Object} orm - Bookbrainz orm wrapper holding all models
+ * @param  {Transaction} transacting - The present knex transacting object
+ * @param  {Object} oldAnnotation - The old annotation object
+ * @param  {string} newContent - New annotation to be set
+ * @param  {Object} revision - The present revision object
+ * @returns {Promise<Object>} - Returns Annotation object
+ */
 export function updateAnnotation(
-	orm, transacting, oldAnnotation, newContent, revision
+	orm: Object, transacting: Transaction, oldAnnotation: ?Object,
+	newContent: string, revision: Object
 ) {
 	const {Annotation} = orm;
 	const oldContent = oldAnnotation && oldAnnotation.get('content');
