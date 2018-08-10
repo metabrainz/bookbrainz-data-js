191de7a86a8e8f9ab88bb0f7ed8e2b2f1c3b024d
Shivam Tripathi <shivamtripathi0108@gmail.com>
2018-08-11 02:07:30 +0530

    feat(func/index): Update func index

--- src/func/index.js
+++ src/func/index.js
@@ -1,14 +1,18 @@
 import * as alias from './alias';
+import * as entity from './entity';
 import * as identifier from './identifier';
 import * as language from './language';
 import * as relationship from './relationship';
 import * as set from './set';
+import {createEntity} from './create-entity';
 import imports from './imports';
 
 
 export default function init() {
 	return {
 		alias,
+		createEntity,
+		entity,
 		identifier,
 		imports,
 		language,
