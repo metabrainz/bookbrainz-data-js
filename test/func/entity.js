a7f551badbc83860e1ed5e25590b7e4886df08e1
Monkey Do <contact@monkeydo.digital>
2019-05-22 16:10:31 +0200

    test: Add test for getEntity including for redirected bbids

--- test/func/entity.js
+++ test/func/entity.js
@@ -17,7 +17,6 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-import _ from 'lodash';
 import bookbrainzData from '../bookshelf';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
@@ -27,7 +26,9 @@ import {truncateTables} from '../../lib/util';
 chai.use(chaiAsPromised);
 const {expect} = chai;
 const {recursivelyGetRedirectBBID, getEntity} = bookbrainzData.func.entity;
-const {Entity, AuthorHeader, bookshelf} = bookbrainzData;
+const {Entity, AliasSet, RelationshipSet, IdentifierSet,
+	Author, Disambiguation, Gender, Editor, EditorType,
+	Revision, Annotation, bookshelf} = bookbrainzData;
 
 const aBBID = faker.random.uuid();
 const bBBID = faker.random.uuid();
@@ -59,7 +60,10 @@ describe('recursivelyGetRedirectBBID', () => {
 
 	after(function truncate() {
 		this.timeout(0); // eslint-disable-line babel/no-invalid-this
-		return truncateTables(bookshelf, ['bookbrainz.entity_redirect']);
+		return truncateTables(bookshelf, [
+			'bookbrainz.entity_redirect',
+			'bookbrainz.entity'
+		]);
 	});
 
 	it('should return a single-step redirected bbid', async function () {
@@ -70,5 +74,142 @@ describe('recursivelyGetRedirectBBID', () => {
 	it('should return a multiple-step redirected bbid', async function () {
 		const redirectedBBID = await recursivelyGetRedirectBBID(bookbrainzData, cBBID);
 		expect(redirectedBBID).to.equal(eBBID);
+	});
+});
+
+describe('getEntity', () => {
+	async function createAuthorWithData(index, bbid) {
+		const editorAttribs = {
+			genderId: 1,
+			id: index,
+			name: `Fnord ${index}`,
+			typeId: 1
+		};
+		const revisionAttribs = {
+			authorId: index,
+			id: index
+		};
+		const authorAttribs = {
+			aliasSetId: index,
+			annotationId: index,
+			bbid,
+			disambiguationId: index,
+			identifierSetId: index,
+			relationshipSetId: index,
+			revisionId: index
+		};
+		new AliasSet({id: index})
+			.save(null, {method: 'insert'});
+		new IdentifierSet({id: index})
+			.save(null, {method: 'insert'});
+		new RelationshipSet({id: index})
+			.save(null, {method: 'insert'});
+		await new Disambiguation({comment: `Test Disambiguation ${index}`,
+			id: index})
+			.save(null, {method: 'insert'});
+		await new Editor(editorAttribs).save(null, {method: 'insert'});
+		await new Revision(revisionAttribs)
+			.save(null, {method: 'insert'});
+		await new Annotation({
+			content: `Test Annotation ${index}`,
+			id: index,
+			lastRevisionId: index
+		})
+			.save(null, {method: 'insert'});
+		await new Entity({bbid, type: 'Author'})
+			.save(null, {method: 'insert'});
+		await new Author(authorAttribs).save(null, {method: 'insert'});
+	}
+
+	before(
+		async () => {
+			const genderData = {
+				id: 1,
+				name: 'test'
+			};
+			const editorTypeData = {
+				id: 1,
+				label: 'test_type'
+			};
+			await Promise.all([
+				new Gender(genderData).save(null, {method: 'insert'}),
+				new EditorType(editorTypeData).save(null, {method: 'insert'})
+			]);
+
+			await createAuthorWithData(1, aBBID);
+			await createAuthorWithData(2, bBBID);
+			await createAuthorWithData(3, cBBID);
+			// Redirect bBBID -> cBBID
+			await bookshelf.knex('bookbrainz.entity_redirect')
+				.insert({source_bbid: bBBID, target_bbid: cBBID});
+		}
+	);
+
+	after(function truncate() {
+		this.timeout(0); // eslint-disable-line babel/no-invalid-this
+		return truncateTables(bookshelf, [
+			'bookbrainz.entity_redirect',
+			'bookbrainz.entity',
+			'bookbrainz.annotation',
+			'bookbrainz.revision',
+			'bookbrainz.author_header',
+			'bookbrainz.editor',
+			'bookbrainz.disambiguation',
+			'bookbrainz.editor_type',
+			'bookbrainz.relationship_set',
+			'bookbrainz.identifier_set',
+			'bookbrainz.alias_set',
+			'musicbrainz.gender'
+		]);
+	});
+
+	it('should return an entity', async function () {
+		const entityJSON = await getEntity(bookbrainzData, 'Author', aBBID);
+		expect(entityJSON).to.have.all.keys([
+			'aliasSetId', 'annotationId', 'areaId',
+			'bbid', 'beginAreaId', 'beginDate', 'beginDay', 'beginMonth',
+			'beginYear', 'dataId', 'defaultAliasId',
+			'disambiguationId', 'endAreaId', 'endDate', 'endDay', 'endMonth',
+			'endYear', 'ended', 'genderId', 'identifierSetId',
+			'master', 'relationshipSetId', 'revisionId',
+			'type', 'typeId'
+		]);
+		expect(entityJSON.bbid).to.equal(aBBID);
+		expect(entityJSON.aliasSetId).to.equal(1);
+		expect(entityJSON.annotationId).to.equal(1);
+		expect(entityJSON.relationshipSetId).to.equal(1);
+		expect(entityJSON.identifierSetId).to.equal(1);
+		expect(entityJSON.revisionId).to.equal(1);
+	});
+
+	it('should return an entity with extra relations', async function () {
+		const relations = ['aliasSet', 'annotation', 'disambiguation',
+			'identifierSet', 'relationshipSet', 'revision'];
+		const entityJSON = await getEntity(bookbrainzData, 'Author', aBBID, relations);
+		expect(entityJSON).to.have.all.keys([
+			'aliasSet', 'aliasSetId', 'annotation', 'annotationId', 'areaId',
+			'bbid', 'beginAreaId', 'beginDate', 'beginDay', 'beginMonth',
+			'beginYear', 'dataId', 'defaultAliasId', 'disambiguation',
+			'disambiguationId', 'endAreaId', 'endDate', 'endDay', 'endMonth',
+			'endYear', 'ended', 'genderId', 'identifierSet', 'identifierSetId',
+			'master', 'relationshipSet', 'relationshipSetId', 'revision', 'revisionId',
+			'type', 'typeId'
+		]);
+		expect(entityJSON.bbid).to.equal(aBBID);
+		expect(entityJSON.aliasSetId).to.equal(1);
+		expect(entityJSON.annotationId).to.equal(1);
+		expect(entityJSON.relationshipSetId).to.equal(1);
+		expect(entityJSON.identifierSetId).to.equal(1);
+		expect(entityJSON.revisionId).to.equal(1);
+	});
+
+	it('should return the target entity for a redirected bbid', async function () {
+		const redirectedEntityJSON = await getEntity(bookbrainzData, 'Author', bBBID);
+		expect(redirectedEntityJSON.bbid).to.equal(cBBID);
+		expect(redirectedEntityJSON.aliasSetId).to.equal(3);
+		expect(redirectedEntityJSON.annotationId).to.equal(3);
+		expect(redirectedEntityJSON.relationshipSetId).to.equal(3);
+		expect(redirectedEntityJSON.identifierSetId).to.equal(3);
+		expect(redirectedEntityJSON.revisionId).to.equal(3);
 	});
 });
