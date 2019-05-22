28b677f6e107ef0e657efa72a904e084ac9158c9
Monkey Do <contact@monkeydo.digital>
2019-05-22 16:16:19 +0200

    docs: cleanup inline docs

--- src/func/entity.js
+++ src/func/entity.js
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2018  Shivam Tripathi
+ * Copyright (C) 2019  Nicolas Pelletier
  * Some parts adapted from bookbrainz-site
  *
  * This program is free software; you can redistribute it and/or modify
@@ -165,7 +166,7 @@ export function getEntityModelByType(orm: Object, type: string): Object {
  * Do a recursive search in case the redirected bbid also redirects, etc.
  * @param {object} orm - the BookBrainz ORM, initialized during app setup
  * @param {string} bbid - The target entity's bbid.
- * @returns {number} The final bbid to redirect to
+ * @returns {string} The final bbid to redirect to
  */
 export async function recursivelyGetRedirectBBID(orm: Object, bbid: string) {
 	const redirectSQLQuery = `SELECT target_bbid FROM bookbrainz.entity_redirect WHERE source_bbid = '${bbid}'`;
@@ -183,7 +184,7 @@ export async function recursivelyGetRedirectBBID(orm: Object, bbid: string) {
  * @param {string} entityType - The entity model name.
  * @param {string} bbid - The target entity's bbid.
  * @param {string[]} relations - Extra model relationships to fetch along with the entity
- * @returns {Promise} The returned Promise returns the entity in JSON format
+ * @returns {Promise} A Promise that resolves to the entity in JSON format
  */
 export async function getEntity(
 	orm: Object, entityType: string, bbid: string, relations: Array<String> = []
